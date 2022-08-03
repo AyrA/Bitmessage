33bcb5ccf12bc9fe70361de69c93be11f4a75e86
Kevin Gut <kevin.gut@bitm.sg>
2022-08-03 22:25:57 +0200

    Add Base58 encoder

---
+++ Global/Base85.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Bitmessage.Global
+{
+    public static class Base85
+    {
+        public static string Encode(byte[] Data)
+        {
+            if (Data is null)
+            {
+                throw new ArgumentNullException(nameof(Data));
+            }
+            return SimpleBase.Base58.Bitcoin.Encode(new ReadOnlySpan<byte>(Data));
+        }
+
+        public static byte[] Decode(string Data)
+        {
+            if (string.IsNullOrWhiteSpace(Data))
+            {
+                throw new ArgumentException($"'{nameof(Data)}' cannot be null or whitespace.", nameof(Data));
+            }
+            return SimpleBase.Base58.Bitcoin.Decode(Data).ToArray();
+        }
+    }
+}
